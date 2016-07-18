735554017f2682e3bd3fcae331a089eeacedf579
agdhruv <dhruv_agarwal@live.com>
2016-07-18 12:30:28 +0530

    Updates few files

--- route.js
+++ route.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var router = express.Router();
 
+//require collection schemas
 var login = require('./Models/login.js');
 var user = require('./Models/user.js');
-
 
 module.exports = router;
