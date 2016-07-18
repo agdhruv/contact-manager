cc52161d4b518e4e6db09e82040dd94c33829c64
agdhruv <dhruv_agarwal@live.com>
2016-07-18 11:55:50 +0530

    Connect to db

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var session = require('express-session');
+
+//Connect to db
+mongoos.connect('mongodb://localhost:27017/contact-manager', function(err){
+	if(!err){
+		console.log("connection successful");
+	}
+	else{
+		console.log(err);
+	}
+});
 
 app.get('/',function(req,res) {
 	res.send("Running");
