95f8faf04f0eaeebf7234e7be94a4f5e82f6a341
agdhruv <dhruv_agarwal@live.com>
2016-07-18 12:04:15 +0530

    Add body-parser

--- route.js
+++ route.js
@@ -1,6 +1,4 @@
-
 var express = require('express');
 var mongoose = require('mongoose');
-var login =require('./Models/login.js');
-var user =require('./Models/user.js');
-
+var login = require('./Models/login.js');
+var user = require('./Models/user.js');
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var session = require('express-session');
+var bodyParser = require('body-parser');
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended:true}));
 
 //Connect to db
 mongoos.connect('mongodb://localhost:27017/contact-manager', function(err){
